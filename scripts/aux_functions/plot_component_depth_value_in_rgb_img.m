8f8a657042c28debfb767291c2d0d14f077d9ec4
Manuel Serra <manuelserranunes@gmail.com>
2018-11-25 14:27:53 +0000

    added aux_functions

---
+++ scripts/aux_functions/plot_component_depth_value_in_rgb_img.m
@@ -0,0 +1,18 @@
+function plot_component_depth_value_in_rgb_img(depth_img, rgb_img, component_indices)
+
+    for j = 1:1
+
+            [y, x] = ind2sub(size(rgb_img(:,:,1)), component_indices);
+
+            for i = 1:length(y)
+                d(i) = depth_img(y(i), x(i));
+                color = interp1([2,5],[0,255],depth_img(y(i), x(i)));
+                rgb_img(y(i), x(i), :) = [color 100 50];
+            end
+    end
+    
+    figure(12)
+    imshow(uint8(rgb_img));
+    
+end
+    
