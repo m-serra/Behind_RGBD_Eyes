275d31be821bbbbd49c76f387b2cb70d0a9f01f3
carlosaleluia <carlos.aleluia7@gmail.com>
2018-12-11 14:49:18 +0000

    added some functions

---
+++ scripts/get_R_T.m
@@ -0,0 +1,81 @@
+function [ R, T ] = get_R_T( grayseq_cam1, grayseq_cam2, dseq_cam1, dseq_cam2, cam_params, ransac_frames )
+
+% matches_3D contains matched points from 2 cams, to calculate R and T.
+% first 3 columns are the coordinates in cam1 referential (columns 1,2,3)
+% last 3 columns are the coordinates in cam2 referential (columns 4,5,6)
+% contains as many rows as matched pair points;
+% For robustness, we will use points from the first and last frame. Note
+% that one frame would be enough, but if the chosen frame has few
+% keypoints, it might be helpful to have a second frame, hopefully with
+% different keypoints
+matches_3D = [];
+
+for frame=ransac_frames
+    % 1.1) Find keypoints for the pair of images (cam1,cam2)
+    % key point detection using SIFT:
+    % check http://www.vlfeat.org/overview/sift.html for detais
+    peak_thresh = 0;
+    edge_thresh = 20;
+    [f_cam1, d_cam1] = vl_sift(single(grayseq_cam1(:,:,frame)),...
+                                'PeakThresh', peak_thresh, 'edgethresh', edge_thresh);
+    [f_cam2, d_cam2] = vl_sift(single(grayseq_cam2(:,:,frame)),...
+                                'PeakThresh', peak_thresh, 'edgethresh', edge_thresh);
+    
+    % 1.2) Match keypoints
+    % we can apply a 1st filter at this stage to remove noisy matches from
+    % the beggining, by reducing the threshold 
+    % Note: vl_ubcmatch returns the indexes of the SIFT identified points,
+    % not the indices of the real points. 
+    % Check http://www.vlfeat.org/overview/sift.html for complete info
+ 
+    match_threshold = 2.5; % 1.5 is the default value
+    [matches, scores] = vl_ubcmatch(d_cam1, d_cam2, match_threshold);
+    matches = round([f_cam1(1:2,matches(1,:))' f_cam2(1:2,matches(2,:))']);
+    
+    % plotting
+    plot_sift(grayseq_cam1(:,:,frame),grayseq_cam2(:,:,frame), matches);
+    
+    % Convert points in 3D coordinates in the corresponding frame
+    u_cam1_rgb = matches(:,1); v_cam1_rgb = matches(:,2);
+    u_cam2_rgb = matches(:,3); v_cam2_rgb = matches(:,4);
+    
+    cam1_3D = rgb_to_xyz([u_cam1_rgb v_cam1_rgb], dseq_cam1(:,:,frame),cam_params);
+    cam2_3D = rgb_to_xyz([u_cam2_rgb v_cam2_rgb], dseq_cam2(:,:,frame),cam_params);
+        
+    matches_3D = [matches_3D; [cam1_3D' cam2_3D']]; 
+    
+end
+
+% 1.3) Filter out outlier matches by applying RANSAC method
+% At this stage we have all the pair points in the variable matches_3D. See
+% variable description next to its first instantiation. 
+
+% S = log(1-P)/log(1-p^k)
+% P = probability of success
+% p = inliers ratio
+% k = minimum points for estimate
+P = 0.99;
+p = 0.5;
+k = 4;
+max_iter = round(log10(1-P) / log10(1-p^k));
+
+inliers = ransac_procrustes(matches_3D, max_iter);
+
+% 1.4) Fit R and T using pair points obtained after applying RANSAC in 1.3
+% We should use PROCRUSTES solution
+[~, ~, tr] = procrustes(inliers(:,1:3), inliers(:,4:6), 'Scaling',false,...
+                        'Reflection',false);
+                    
+% Checking total error:
+predicted_cam1 = matches_3D(:,4:6)*tr.T + ...
+                            repmat(tr.c(1,:),size(matches_3D, 1),1);
+diff = matches_3D(:,1:3) - predicted_cam1;
+error = sqrt(sum(diff.^2,2));
+total_wrong_matches = sum(double(error>0.25));
+
+R = tr.T';
+T = tr.c(1,:)';
+
+
+end
+
---
+++ scripts/track3D_part2.m
@@ -0,0 +1,85 @@
+function [ objects ] = track3D_part2( imagesequence_cam1, imagesequence_cam2, cam_params )
+% Part II
+
+% In this section we will identify moving objects not just with one camera 
+% but with two cameras. Therefore, we must match information obtained by 
+% each camera and gather all the components identified.
+% As we want to match objects, we must represent them in the same
+% coordinate frame, which we will choose to be the referential of cam1.
+% Our job is decomposed in three stages:
+% 1: Identify R, T from cam2 to cam1 referential
+% 2: Identify components on cam1 and cam2 images
+% 3. Use matrices calculated on step 1 to merge components found on step 2
+
+
+% Load images
+[rgbseq_cam1, grayseq_cam1, dseq_cam1] = load_images(imagesequence_cam1);
+[rgbseq_cam2, grayseq_cam2, dseq_cam2] = load_images(imagesequence_cam2);
+
+% some auxiliary variables
+n_frames = size(dseq_cam1,3); % number of frames in sequence
+size_dimg = size(dseq_cam1(:,:,1)); % stores size of dimg
+
+% Stage 1: Identify R, T from cam2 to cam1 referential
+ransac_frames = 1;
+[R, T] = get_R_T(grayseq_cam1, grayseq_cam2, dseq_cam1, dseq_cam2, cam_params, ransac_frames);
+
+% Subtract background
+background_cam1 = get_background(dseq_cam1);
+background_cam2 = get_background(dseq_cam2);
+
+% --- Start working with components ---
+
+% cell array that contains COMPONENTS FOR ALL FRAMES
+objects = [];
+
+% parameters
+diff_threshold = 0.2;
+filter_size = 10;
+
+% iterate over frames:
+for frame=1:size(dseq_cam1,3)
+    % get components for a given frame
+    cc_cam1 = get_components(background_cam1, dseq_cam1(:,:,frame), diff_threshold, ...
+                        filter_size, rgbseq_cam1(:,:,:,frame));
+                    
+    cc_cam2 = get_components(background_cam2, dseq_cam2(:,:,frame), diff_threshold, ...
+                        filter_size, rgbseq_cam2(:,:,:,frame));
+                    
+    %get component info: descriptor, X, Y, Z
+    frame_components_cam1 = get_component_info(cc_cam1, dseq_cam1(:,:,frame), ...
+                        rgbseq_cam1(:,:,:,frame), cam_params, frame);
+                    
+    frame_components_cam2 = get_component_info(cc_cam2, dseq_cam2(:,:,frame), ...
+                        rgbseq_cam2(:,:,:,frame), cam_params, frame);
+                    
+    %transform objects from cam2 into cam1 reference
+    frame_components_cam2 = transform_object(frame_components_cam2, R, T, n_frames);
+    
+    
+    frame_components_new = registration( frame_components_cam1, frame_components_cam2, frame);
+    
+    
+    % compare frame_components's components
+    % with previous frame components
+    if frame == 1 %first frame -> no old components
+        frame_components_old = frame_components_new;
+        continue
+    else
+        [new_objects, frame_components_old] = match_components(frame_components_old, frame_components_new);
+    end
+    %this will be inside a loop with frame iterator
+    objects = [objects new_objects];
+    if frame == size(dseq,3) %last frame -> all old components become new_objects
+        [new_objects, ~] = match_components(frame_components_old, []);
+        objects = [objects new_objects];
+    end
+    
+end
+
+% take from the objects structure the components that only appear in one
+% frame (according to the professor, those components are not meaningful)
+objects = clean_single_objects(objects);
+
+end
+
